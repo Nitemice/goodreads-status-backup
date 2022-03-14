b7b03c7e32e16a48863ef6731ed5b4f1d17f3719
Nitemice <webmaster@nitemice.net>
2022-03-14 15:25:40 +1100

    Stripped down to status retrieval functionality
    Removed all Goodreads API functionality, since it doesn't work anymore.

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# backup-goodreads
+# goodreads-status-backup
 
-This is a Python script for backing up your reviews, shelves and statuses from 
-Goodreads.
-It is based on both 
-[bjaanes/goodreads-backup](https://github.com/bjaanes/goodreads-backup) and 
-[alexwlchan/backup-goodreads](https://github.com/alexwlchan/backup-goodreads).
+This is a Python script for backing up your statuses from Goodreads.
+
+It is spun out of
+[Nitemice/goodreads-backup](https://github.com/Nitemice/goodreads-backup).
+That script no longer works (for new users, at least), due to changes to the
+Goodreads API.
 
 ## Installation
 
@@ -12,31 +13,34 @@ This script requires Python 3. Create a virtualenv and install dependencies:
 
 ```shell
 $ virtualenv env
-$ source env/bin/activate
+$ source env/Scripts/activate
 $ pip install -r requirements.txt
 ```
 
-You need to set up three things before you can use the script:
+You need to set up two things before you can use the script:
 
-1.  Make your Goodreads reviews public. This script only uses the basic
-    API, not OAuth, and so private reviews can't be backed up.
+1.  Make your Goodreads statuses public. This script only scrapes public pages,
+    so private statuses can't be backed up.
 2.  Get your Goodreads user ID. This is the 8-digit number in the URL of
     your profile page. For example, if your user page is
     `https://www.goodreads.com/user/show/12345678-john-smith`, then your
     user ID is `12345678`.
-3.  Get a [developer API key](https://www.goodreads.com/api/keys) from
-    the Goodreads website.
 
 ## Usage
 
-Run the script, passing your user ID and API key as command-line flags:
+Run the script, passing your user ID as a command-line flag:
 
 ```shell
-$ python backup_goodreads.py --user-id=12345678 --api-key=abcdefg123
+$ python backup_goodreads.py --user-id=12345678
 ```
-Alternatively, these details can be set using `keyring`.
-See [its documentation](https://pypi.org/project/keyring/#command-line-utility)
-for how this is done.  
+
+Alternatively, a config file can be used to specify these details.
+See below for an example:
+```json
+{
+    "user_id": "12345678"
+}
+```
 
 To see other options, run with the ``--help`` flag:
 
--- backup_goodreads.py
+++ backup_goodreads.py
@@ -1,84 +1,56 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
-"""A script for backing up your Goodreads library."""
+"""A script for backing up your Goodreads statuses."""
 
 import argparse
 import itertools
 import json
-
-from datetime import timezone, datetime
-from xml.etree import ElementTree
-from bs4 import BeautifulSoup
 import re
-
-import keyring
-import requests
-
-import csv
 import os
+from datetime import datetime
+
+from bs4 import BeautifulSoup
+import requests
 
 
 def read_config():
     """Returns configuration for using the script.
 
     Configuration is read from one of two places:
-     1. The system keychain
-     2. Command-line arguments
+     1. Command-line arguments
+     2. A config file
 
-    Command-line arguments take precedence over keychain values.  If the
-    keychain values are empty/missing, the command-line switches are required.
+    Command-line arguments take precedence over config file values.  If the
+    config file is empty/missing, the command-line switches are required.
 
     """
-    # Read some initial config from the system keychain: if this doesn't
-    # exist, then we read it from command-line arguments.
-    user_id = keyring.get_password('goodreads', 'user_id')
-    api_key = keyring.get_password('goodreads', 'api_key')
+    # Attempt to read config from the config file.
+    # We will use this if there is no command-line argument.
+    user_id = None
+    try:
+        with open("config.json", "r") as config_file:
+            config_data = json.load(config_file)
+            user_id = config_data.user_id
+    except FileNotFoundError:
+        pass
 
     parser = argparse.ArgumentParser(
-        description='A script to back up data from Goodreads.')
+        description='A script to back up status data from Goodreads.')
 
-    parser.add_argument(
-        '-o', '--output', default=os.getcwd(),
-        help='output path for backup files')
-    parser.add_argument(
-        '--user-id', required=(user_id is None),
-        help='Goodreads user ID')
-    parser.add_argument(
-        '--api-key', required=(api_key is None),
-        help='Goodreads API key (https://www.goodreads.com/api/keys)')
-    parser.add_argument(
-        '-m', '--mode', nargs='+', default=['reviews', 'shelves', 'statuses'],
-        help='which modes of the script to run')
+    parser.add_argument('-o',
+                        '--output',
+                        default=os.getcwd(),
+                        help='output path for backup files')
+    parser.add_argument('--user-id',
+                        required=(user_id is None),
+                        help='Goodreads user ID')
 
     config = vars(parser.parse_args())
 
     if config['user_id'] is None:
         config['user_id'] = user_id
-    if config['api_key'] is None:
-        config['api_key'] = api_key
 
     return config
-
-
-def identity(x):
-    """Identity function."""
-    return x.text
-
-
-def convert_date_from_api(element):
-    """Convert a date string from the Goodreads API into an ISO-8601 UTC
-    string."""
-    # We may get ``None`` if the API is missing any information for a
-    # particular date field -- for example, the ``date_read`` field is
-    # only filled in when a book is read.
-    date_str = element.text
-    if date_str is None:
-        return None
-    else:
-        # In the API responses, dates are returned in the form:
-        # "Mon Oct 24 12:26:31 -0700 2016"
-        date_obj = datetime.strptime(date_str, '%a %b %d %H:%M:%S %z %Y')
-        return str(date_obj.astimezone(timezone.utc))
 
 
 def convert_date_from_page(element):
@@ -89,51 +61,6 @@ def convert_date_from_page(element):
     # "Oct 24, 2016 12:26PM"
     date_obj = datetime.strptime(date_str, '%b %d, %Y %I:%M%p')
     return str(date_obj)
-
-
-def convert_rating(element):
-    """Convert a rating from the Goodreads API."""
-    rating = element.text
-
-    # The Goodreads API returns '0' to indicate an unrated book;
-    # make this a proper null type.
-    if rating == '0':
-        return None
-    else:
-        return int(rating)
-
-
-def convert_authors(element):
-    """Get the names of the authors for this book."""
-    # The Goodreads API returns information about authors in an <authors>
-    # element on the <review> element, in the following format:
-    #
-    #    <authors>
-    #      <author>
-    #        <id>1234</id>
-    #        <name>John Smith</name>
-    #        <snip>...</snip>
-    #      </author>
-    #      <author>
-    #        <id>5678</id>
-    #        <name>Jane Doe</name>
-    #        <snip>...</snip>
-    #      </author>
-    #    </authors>
-    #
-    return [author.find('name').text for author in element.findall('author')]
-
-
-def convert_shelves(element):
-    """Get the names of all the shelves that hold this book."""
-    # The shelves for a review are returned in the following format:
-    #
-    #    <shelves>
-    #      <shelf name="read" exclusive="true"/>
-    #      <shelf name="fiction" exclusive="false" review_shelf_id="1234"/>
-    #    </shelves>
-    #
-    return [shelf.attrib['name'] for shelf in element.findall('shelf')]
 
 
 def convert_page_count(element):
@@ -152,60 +79,8 @@ def convert_status_body(element):
     return ''.join(map(str, element.contents)).strip()
 
 
-# Map from <review> tag name to pair (output key, converter)
-REVIEW_TAGS = {
-    'read_at':      ('date_read',   convert_date_from_api),
-    'date_added':   ('date_added',  convert_date_from_api),
-    'body':         ('review',      convert_body),
-    'rating':       ('my_rating',   convert_rating),
-    'shelves':      ('bookshelves', convert_shelves)
-}
-
-# Map from <book> tag name to pair (output key, converter)
-BOOK_TAGS = {
-    'authors':          ('authors',             convert_authors),
-    'id':               ('book_id',             identity),
-    'title':            ('title',               identity),
-    'isbn':             ('isbn',                identity),
-    'isbn13':           ('isbn13',              identity),
-    'average_rating':   ('average_rating',      identity),
-    'publisher':        ('publisher',           identity),
-    'format':           ('binding',             identity),
-    'num_pages':        ('page_count',          convert_page_count),
-    'publication_year': ('publication_year',    identity),
-    'published':        ('orig_year_published', identity),
-}
-
-
-def _get_data_from_goodreads_api(user_id, api_key, page_no, page_size=200):
-    """Retrieve data about the reviews of a given user from the Goodreads API.
-    Returns a Request object if successful, raises an Exception if not.
-
-    :param user_id: The ID of the Goodreads user.
-    :param api_key: The Goodreads API key.
-    :param page_no: Which page of reviews to fetch.
-    :param page_size: How many reviews on each page.
-
-    """
-    # reviews.list (https://www.goodreads.com/api/index#reviews.list) gets
-    # all the books on somebody's shelf.
-    req = requests.get('https://www.goodreads.com/review/list.xml', params={
-        'v': '2',
-        'key': api_key,
-        'id': user_id,
-        'page': str(page_no),
-        'per_page': str(page_size),
-    })
-    if req.status_code != 200:
-        raise Exception(
-            'Unexpected error code from Goodreads API: %s\n'
-            'Error message: %r' % (req.status_code, req.text)
-        )
-    return req
-
-
 def _get_data_from_goodreads_site(user_id, page_no):
-    """Retrieve data about the statuses of a given user from the Goodreads API.
+    """Retrieve data about the statuses of a given user from the Goodreads site.
     Returns a Request object if successful, raises an Exception if not.
 
     :param user_id: The ID of the Goodreads user.
@@ -213,72 +88,12 @@ def _get_data_from_goodreads_site(user_id, page_no):
 
     """
     # user_status.list gets all the statuses by a user.
-    req = requests.get('https://www.goodreads.com/user_status/list/' + user_id, params={
-        'page': str(page_no)
-    })
+    req = requests.get('https://www.goodreads.com/user_status/list/' + user_id,
+                       params={'page': str(page_no)})
     if req.status_code != 200:
-        raise Exception(
-            'Unexpected error code from Goodreads API: %s\n'
-            'Error message: %r' % (req.status_code, req.text)
-        )
+        raise Exception('Unexpected error code from Goodreads API: %s\n'
+                        'Error message: %r' % (req.status_code, req.text))
     return req
-
-
-def _get_reviews_from_api(user_id, api_key):
-    """Generates <review> elements from the API data.
-
-    :param user_id: The ID of the Goodreads user.
-    :param api_key: The Goodreads API key.
-
-    """
-    for page_no in itertools.count(1):
-        req = _get_data_from_goodreads_api(
-            user_id=user_id, api_key=api_key, page_no=page_no
-        )
-        reviews = ElementTree.fromstring(req.text).find('reviews')
-        for r in reviews:
-            yield r
-        if int(reviews.attrib['end']) >= int(reviews.attrib['total']):
-            break
-
-
-def get_reviews(user_id, api_key):
-    """Generate reviews associated with a Goodreads user as dictionaries.
-
-    :param user_id: The ID of the Goodreads user.
-    :param api_key: The Goodreads API key.
-
-    """
-    for review in _get_reviews_from_api(user_id, api_key=api_key):
-        data = {}
-
-        def convert(element, tag_mapping):
-            if element.tag in tag_mapping:
-                key, converter = tag_mapping[element.tag]
-                data[key] = converter(element)
-
-        for review_elt in review:
-            if review_elt.tag == 'book':
-                for book_elt in review_elt:
-                    convert(book_elt, BOOK_TAGS)
-            else:
-                convert(review_elt, REVIEW_TAGS)
-        yield data
-
-
-def extract_shelves(reviews):
-    """Sort review data into shelves dictionary.
-
-    :param reviews: A list of book reviews.
-
-    """
-    shelves = {}
-    for book in reviews:
-        for shelf in book['bookshelves']:
-            if shelf not in shelves:
-                shelves[shelf] = []
-            shelves[shelf].append(book)
-    return shelves
 
 
 def _extract_status_from_element(element):
@@ -318,16 +133,20 @@ def _extract_status_from_element(element):
     #
     #   </div>
 
-    status['status'] = convert_status_body(content.find('div', {'class': 'readable body'}))
+    status['status'] = convert_status_body(
+        content.find('div', {'class': 'readable body'}))
 
-    date_id_element = content.find('span', {'class': 'greyText uitext smallText'}).a
+    date_id_element = content.find('span', {
+        'class': 'greyText uitext smallText'
+    }).a
     status['date'] = convert_date_from_page(date_id_element)
     status['status_id'] = date_id_element['href'].split('/')[-1]
 
     progress_element = content.find('span', {'class': 'user_status_header'})
     book_title_link = progress_element.find('a', {'rel': 'nofollow'})
     status['book_title'] = convert_body(book_title_link)
-    status['book_id'] = re.split('[-.]', book_title_link['href'].split('/')[-1])[0]
+    status['book_id'] = re.split('[-.]',
+                                 book_title_link['href'].split('/')[-1])[0]
 
     # The progress string can be in one of four formats:
     # 1: User is Z% done with Book_Title
@@ -340,8 +159,9 @@ def _extract_status_from_element(element):
     # If we get #3, we can assume no progress yet (0% done).
     # If we get #4, we know they're finished, so 100% done.
     progress_text = convert_body(progress_element)
-    progress_values = re.search('.* is (?:on page )?(\d*%?) (?:of (\d*) of|done with) .*',
-                                progress_text, re.S)
+    progress_values = re.search(
+        '.* is (?:on page )?(\d*%?) (?:of (\d*) of|done with) .*',
+        progress_text, re.S)
 
     if not progress_values:
         if 'finished' in progress_text:
@@ -357,7 +177,8 @@ def _extract_status_from_element(element):
         # Format #2
         status['page_no'] = int(progress_values.group(1))
         status['total_pages'] = int(progress_values.group(2))
-        status['percentage'] = int(int(status['page_no']) / int(status['total_pages']) * 100)
+        status['percentage'] = int(
+            int(status['page_no']) / int(status['total_pages']) * 100)
 
     return status
 
@@ -384,7 +205,8 @@ def get_statuses(user_id):
 
     # Iterate til we have all the statuses
     while len(statuses) < total_status_count:
-        req = _get_data_from_goodreads_site(user_id=user_id, page_no=next(page_no))
+        req = _get_data_from_goodreads_site(user_id=user_id,
+                                            page_no=next(page_no))
         soup = BeautifulSoup(req.text, "html.parser")
 
         # Extract all statuses from this page
@@ -410,54 +232,6 @@ def _dump_json_to_disk(data, output_dir, filename):
         f.write(json_str)
 
 
-def write_reviews_to_disk(reviews, output_dir, keep_empty=False):
-    """Writes review data to disk as JSON file.
-
-    :param reviews: The review data to dump to file.
-    :param output_dir: The directory where the file will be created.
-    :param keep_empty: If reviews without a rating nor review text should be
-                       included in the file.
-
-    """
-    filtered_reviews = reviews
-    if not keep_empty:
-        filtered_reviews = [d for d in filtered_reviews if d['my_rating'] or d['review']]
-    _dump_json_to_disk(filtered_reviews, output_dir, 'goodreads_reviews.json')
-
-
-def write_shelves_to_disk(shelves, output_dir, header=True):
-    """Writes shelves data to disk as CSV files.
-
-    :param shelves: The shelves data to dump to CSV.
-    :param output_dir: The directory where the CSV files will be created.
-    :param header: If a header line should be included in the file.
-
-    """
-    # Fields to write to disk.
-    shelf_keys = [
-        'book_id',
-        'title',
-        'authors',
-        'isbn',
-        'isbn13',
-        'my_rating',
-        'date_added',
-        'date_read'
-    ]
-
-    for shelf_name, shelf in shelves.items():
-        filename = os.path.join(output_dir, shelf_name + '.csv')
-        with open(filename, 'w', newline='', encoding='utf-8') as csv_file:
-            csv_writer = csv.writer(csv_file, delimiter=',')
-
-            if header:
-                csv_writer.writerow(shelf_keys)
-
-            for book in shelf:
-                filtered_book = {key: book[key] for key in shelf_keys}
-                csv_writer.writerow(list(filtered_book.values()))
-
-
 def write_statuses_to_disk(statuses, output_dir):
     """Writes status data to disk as JSON file.
 
@@ -470,25 +244,9 @@ def write_statuses_to_disk(statuses, output_dir):
 
 def main():
     cfg = read_config()
-
-    if 'reviews' in cfg['mode'] or 'shelves' in cfg['mode']:
-        """Parse the Goodreads API and grab all reviews."""
-        reviews = get_reviews(user_id=cfg['user_id'], api_key=cfg['api_key'])
-        reviews_list = list(reviews)
-
-        if 'reviews' in cfg['mode']:
-            """Save the reviews to disk."""
-            write_reviews_to_disk(reviews_list, cfg['output'])
-
-        if 'shelves' in cfg['mode']:
-            """Save all the shelves to disk."""
-            shelves = extract_shelves(reviews_list)
-            write_shelves_to_disk(shelves, cfg['output'])
-
-    if 'statuses' in cfg['mode']:
-        """Parse the Goodreads site and grab all statuses."""
-        statuses = get_statuses(cfg['user_id'])
-        write_statuses_to_disk(statuses, cfg['output'])
+    """Parse the Goodreads site and grab all statuses."""
+    statuses = get_statuses(cfg['user_id'])
+    write_statuses_to_disk(statuses, cfg['output'])
 
 
 if __name__ == '__main__':
